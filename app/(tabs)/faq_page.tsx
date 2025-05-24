4d0afb1a3baefe0291060bb62628abc89793af13
celestinebuendia <celestinembuendia@yahoo.com>
2025-05-24 04:58:02 -0700

    FAQ page content and styling

--- app/(tabs)/faq_page.tsx
+++ app/(tabs)/faq_page.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import BasicButton from "@/components/BasicButton";
 import AboutPopup from "@/components/AboutPopup";
 import { useState } from "react";
@@ -8,26 +8,33 @@ export default function AboutScreen() {
   const [isAboutVisible, setIsAboutVisible] = useState<boolean>(false);
 
   return (
-    <View style={styles.container}>
-      <Dropdown 
-        header="Error: 'Please allow location permission'"
-        image={require('../../assets/images/faq_location.png')}
-        text="If no location settin was selected on startup of the app, you need to go into your phone’s Settings and “Allow location permissions” for this app."
-      />
-      <AboutPopup isVisible={isAboutVisible} onClose={() => setIsAboutVisible(false)}/>
-      <BasicButton label="About Us" onPress={() => setIsAboutVisible(true)}/>
-    </View>
+    <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <View style={styles.container}>
+        <Dropdown 
+          header="Error: 'Please allow location permission'"
+          image={require('../../assets/images/faq_location.png')}
+          text="If no location setting was selected on startup of the app, you need to go into your phone’s Settings and “Allow location permissions” for this app."
+        />
+        <Dropdown 
+          header="Is it possible to give feedback on the app?"
+          text="Since the app is not yet deployed on the App Store or Google Play Store, any feedback can be directed to any one of us using our GitHub emails!"
+        />
+        <Dropdown 
+          header="I can’t see any resources in my area?"
+          text="The app is currently designed with limited resource data, primarily within the Seattle area. We are working to expand our app in the future!"
+        />
+        <AboutPopup isVisible={isAboutVisible} onClose={() => setIsAboutVisible(false)}/>
+        <BasicButton label="About Us" onPress={() => setIsAboutVisible(true)}/>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "white",
     alignItems: "center",
+    flexGrow: 1,
     padding: 32,
-  },
-  text: {
-    color: "#000",
   },
 });
--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,30 +1,101 @@
-import { useState } from "react";
-import { Image, ImageSourcePropType, StyleSheet, Text, View } from "react-native";
+import colors from "@/app/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useEffect, useRef, useState } from "react";
+import { Animated, Image, ImageSourcePropType, Pressable, StyleSheet, Text, View } from "react-native";
 
 type Props = {
   header: string;
-  image: ImageSourcePropType;
+  image?: ImageSourcePropType;
   text: string;
 };
 
 export default function Dropdown({ header, image, text}: Props) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [contentHeight, setContentHeight] = useState<number>(0);
+
+  const animation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      Animated.timing(animation, {
+        toValue: isOpen ? 1 : 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }, 50)
+  }, [isOpen])
+
+  const animatedHeight = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, contentHeight],
+  });
 
   return (
-    <View>
-      <View>
-        <Text>{header}</Text>
+    <Pressable onPress={() => setIsOpen(!isOpen)} style={styles.container}>
+      <View style={styles.header}>
+        <View style={{ flex: 1, marginRight: 10 }}>
+          <Text style={styles.headerText}>{header}</Text>
+        </View>
+        <MaterialIcons name="expand-more" color={'white'} size={40}/>
       </View>
-      <View>
-        <Image source={image} resizeMode="contain"/>
-        <Text>{text}</Text>
-      </View>
-    </View>
-    );
+        <Animated.View style={{ height: animatedHeight, overflow: 'hidden' }}>
+          <View 
+            style={styles.content}
+            onLayout={event => {
+              const height = event.nativeEvent.layout.height;
+              setContentHeight(height);
+            }}
+          >
+            {image ? (
+              <Image source={image} resizeMode="contain" style={styles.img}/>
+            ):(
+              <View style={{height: 1}}/>
+            )}
+            <Text style={styles.text}>{text}</Text>
+          </View>
+        </Animated.View>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    backgroundColor: colors.lightGray,
+    borderRadius: 10,
+    width: '100%',
+    marginBottom: 25,
   },
+  header: {
+    backgroundColor: colors.navyBlue,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  headerText: {
+    fontWeight: '700',
+    color: 'white',
+    fontSize: 20,
+  },
+  content: {
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomLeftRadius: 10, 
+    borderBottomRightRadius: 10,
+  },
+  img: {
+    width: '65%',
+    height: 200,
+    marginBottom: 10,
+  },
+  text: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    fontSize: 14,
+    fontWeight: '700',
+  }
 });
