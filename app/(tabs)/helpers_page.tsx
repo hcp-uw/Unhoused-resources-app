c51ff294728af1140f562a7c11f751d33bd73f47
Leonardo Paredes <leonaa2@uw.edu>
2025-05-27 11:29:13 -0700

    Basic helpers page redone completely from scratch with awesome Dropdown.tsx component from team

--- app/(tabs)/helpers_page.tsx
+++ app/(tabs)/helpers_page.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+import Dropdown from "@/components/Dropdown";
 
 export default function HelpersPage() {
+  const [isAboutVisible, setIsAboutVisible] = useState<boolean>(false);
+
   return (
-    <View>
-      <Text>helpers_page</Text>
-    </View>
-  )
+    <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <View style={styles.container}>
+        <Dropdown 
+          header="Volunteering"
+          text="For volunteering, please visit each resource's respective website! They likely have a “volunteer” page. Example: https://rootsinfo.org/volunteernow"
+        />
+        <Dropdown 
+          header="Donations"
+          text="Donations are not yet in this app! If you would like to donate, go to a resource's respective website! Example: https://rootsyoungadultshelter-bloom.kindful.com/"
+        />
+        <Dropdown 
+          header="Learn"
+          text="Common resources include: Charity Navigator for seeing charity ratings and reliability King County Regional Homelessness Authority for government based information https://kcrha.org/"
+        />
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    alignItems: "center",
+    flexGrow: 1,
+    padding: 32,
+  },
+});
