fc93cbf8b7206a2cb10dbe44719034ff9ead6161
Leonardo Paredes <leonaa2@uw.edu>
2025-05-27 12:27:09 -0700

    Added loading display to wait for location to be loaded! Else app just throws lots o errors before its ready

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,44 @@
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Text, View, StyleSheet, ScrollView, ActivityIndicator } from "react-native";
 
 import colors from "../../utils/colors";
 import WeatherView from "@/components/WeatherView";
 import SelectButton from "@/components/SelectButton";
+import { useLocationData } from "@/utils/locationContext";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
-  return (
-    <ScrollView contentContainerStyle={{flexGrow: 1}}>
-      <View style={styles.appScreen}> 
-        <View style={styles.searchSelection}>
-          <Text style={styles.header}>{"What are you\nlooking for?"}</Text>
-          <View style={styles.buttonsContainer}>
-            <SelectButton label="Hygiene" source={require('../../assets/images/button_hygiene.png')}/>
-            <SelectButton label="Food" source={require('../../assets/images/button_food.png')}/>
-          </View>
-          <View style={styles.buttonsContainer}>
-            <SelectButton label="Medical" source={require('../../assets/images/button_medical.png')}/>
-            <SelectButton label="Shelter" source={require('../../assets/images/button_shelter.png')}/>
-          </View>
-        </View>
-        <View style={styles.weatherSection}>
-          <Text style={styles.header}>Weather Alerts</Text>
-          <WeatherView />
-        </View>
+  const location = useLocationData();
+  if (!location) {
+    return (
+      <View>
+        <Text style={{fontSize: 30, justifyContent: "center", paddingBottom: 25, marginLeft: 55}}>Loading location...</Text>
+        <ActivityIndicator size="large" style={styles.loader} />
       </View>
-    </ScrollView>
-  );
+    );
+  } else {
+    return (
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={styles.appScreen}> 
+          <View style={styles.searchSelection}>
+            <Text style={styles.header}>{"What are you\nlooking for?"}</Text>
+            <View style={styles.buttonsContainer}>
+              <SelectButton label="Hygiene" source={require('../../assets/images/button_hygiene.png')}/>
+              <SelectButton label="Food" source={require('../../assets/images/button_food.png')}/>
+            </View>
+            <View style={styles.buttonsContainer}>
+              <SelectButton label="Medical" source={require('../../assets/images/button_medical.png')}/>
+              <SelectButton label="Shelter" source={require('../../assets/images/button_shelter.png')}/>
+            </View>
+          </View>
+          <View style={styles.weatherSection}>
+            <Text style={styles.header}>Weather Alerts</Text>
+            <WeatherView />
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -64,5 +75,10 @@ const styles = StyleSheet.create({
   weatherSection: {
     width: '100%',
     maxWidth: 400,
-  }
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
--- app/(tabs)/map_page.tsx
+++ app/(tabs)/map_page.tsx
@@ -4,8 +4,10 @@ import MapView, { Marker, Region } from "react-native-maps";
 import * as Location from "expo-location";
 import { useResourceData } from "@/utils/ResourceContext";
 import { openMap } from "@/components/MapLinker";
+import { useLocationData } from "@/utils/locationContext";
 
 export default function App() {
+    const [location, setLocation] = useState<Location.LocationObject | undefined>();
     const [region, setRegion] = useState<Region | null>(null);
     const [loading, setLoading] = useState(true);
     const resourceRows = useResourceData();
@@ -19,22 +21,29 @@ export default function App() {
                     "Enable location services to use this feature."
                 );
                 setLoading(false);
-                return;
             }
-
-            let userLocation = await Location.getCurrentPositionAsync({});
-            const { latitude, longitude } = userLocation.coords;
-
-            setRegion({
-                latitude,
-                longitude,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01,
-            });
-
-            setLoading(false);
+            // Need to use locationContext, app loads very slowly without it
+            //let userLocation = await Location.getCurrentPositionAsync({});
         })();
     }, []);
+
+    let userLocation = useLocationData();
+    useEffect(() => {
+        if (!userLocation) {
+            // Still loading
+            return;
+        }
+        const { latitude, longitude } = userLocation.coords;
+
+        setRegion({
+            latitude,
+            longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+        });
+
+        setLoading(false);
+    }, [location]);
 
     if (loading || !region) {
         return <ActivityIndicator size="large" style={styles.loader} />;
